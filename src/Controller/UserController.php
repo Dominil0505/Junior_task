d201bc862342b8b3b3bf2d3182cc6f67375aad3a
ALIZ-PC\Domi <dominik.szabo.03@gmail.com>
2023-06-27 20:09:39 +0200

    Save data to database

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -15,9 +19,42 @@ class UserController extends AbstractController
         $user = new User();
 
         $form = $this->createForm(UserFormType::class, $user);
-
+        
         return $this->render('user/form.html.twig', [
-            'form' => $form
+            'form' => $form->createView()
         ]);
+    }
+
+    
+    #[Route('/send', name: 'send_form', methods: 'POST')] 
+    public function submitForm(Request $request, EntityManagerInterface $entityManager) : Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        
+        if($form->isSubmitted() && $form->isValid())
+        { 
+            $name = $form->get('Name')->getData();
+            $email = $form->get('Email')->getData();
+            $message = $form->get('Message')->getData();
+
+            $user->setName($name);
+            $user->setEmail($email);
+            $user->setMessage($message);
+
+            // save data to database
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $responseMesagge = "<h1>Köszönjük szépen a kérdésedet. Válaszunkkal hamarosan keresünk a megadott e-mail címen.</h1>";
+        }
+        else {
+            $responseMesagge = '<h1 class="warning-message"> Hiba! Kérjük töltsd ki az összes mezőt! </h1>';
+        }
+    
+        return new Response($responseMesagge);
     }
 }
